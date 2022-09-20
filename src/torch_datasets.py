1ecabc93f6ddcd03552662f9ac8acff5171b4b8d
gunesevitan <gunesevitan@gmail.com>
2022-09-20 20:21:04 +0300

    dataset

---
+++ src/torch_datasets.py
@@ -0,0 +1,77 @@
+import cv2
+import torch
+from torch.utils.data import Dataset
+
+import image_utils
+
+
+class ClassificationDataset(Dataset):
+
+    def __init__(self, image_paths, labels, tile_size, n_tiles, transforms=None):
+
+        self.image_paths = image_paths
+        self.labels = labels
+        self.tile_size = tile_size
+        self.n_tiles = n_tiles
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+
+        """
+        Get the idxth element in the dataset
+
+        Parameters
+        ----------
+        idx (int): Index of the sample (0 <= idx < len(self.image_paths))
+
+        Returns
+        -------
+        tiles (torch.FloatTensor of shape (n_tiles, 3, height, width)): Tiles tensor
+        label (torch.FloatTensor of shape (1)): Label tensor
+        """
+
+        image = cv2.imread(str(self.image_paths[idx]))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        tiles = image_utils.tile_image(
+            image=image,
+            tile_size=self.tile_size,
+            n_tiles=self.n_tiles
+        )
+        del image
+        print(tiles.shape)
+
+        if self.labels is not None:
+
+            label = self.labels[idx]
+            label = torch.as_tensor(label, dtype=torch.long)
+
+            if self.transforms is not None:
+                # Apply transforms to tiles and stack them along the batch dimension
+                tiles = [self.transforms(image=tile)['image'].float() for tile in tiles]
+                tiles = torch.stack(tiles, dim=0)
+            else:
+                tiles = [torch.as_tensor(tile, dtype=torch.float) for tile in tiles]
+                tiles = torch.stack(tiles, dim=0)
+                tiles = torch.permute(tiles, dims=(0, 3, 1, 2))
+                # Scale pixel values by max 8 bit pixel value
+                tiles /= 255.
+
+            return tiles, label
+
+        else:
+
+            if self.transforms is not None:
+                # Apply transforms to tiles and stack them along the batch dimension
+                tiles = [self.transforms(image=tile)['image'].float() for tile in tiles]
+                tiles = torch.stack(tiles, dim=0)
+            else:
+                tiles = [torch.as_tensor(tile, dtype=torch.float) for tile in tiles]
+                tiles = torch.stack(tiles, dim=0)
+                tiles = torch.permute(tiles, dims=(0, 3, 1, 2))
+                # Scale pixel values by max 8 bit pixel value
+                tiles /= 255.
+
+            return tiles
