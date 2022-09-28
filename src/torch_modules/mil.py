fab7a48b0652434fb0599c2dcc7fef7e9d5dfd9a
gunesevitan <gunesevitan@gmail.com>
2022-09-28 18:35:28 +0300

    transformer wrapper

--- src/torch_modules/mil.py
+++ src/torch_modules/mil.py
@@ -6,14 +6,14 @@ import timm
 from .weight_initialization import initialize_weights
 
 
-class ClassificationHead(nn.Module):
+class ConvolutionalClassificationHead(nn.Module):
 
     def __init__(self,
                  input_features, intermediate_features, n_classes, pooling_type,
                  activation, activation_args, dropout_probability=0., batch_normalization=False,
                  initialization_args=None):
 
-        super(ClassificationHead, self).__init__()
+        super(ConvolutionalClassificationHead, self).__init__()
 
         self.pooling_type = pooling_type
         self.classifier = nn.Sequential(
@@ -47,23 +47,49 @@ class ClassificationHead(nn.Module):
         return output
 
 
-class MultiInstanceLearningModel(nn.Module):
+class TransformerClassificationHead(nn.Module):
 
-    def __init__(self, n_instances, model_module, model_name, pretrained, head_args):
+    def __init__(self,
+                 input_features, intermediate_features, n_classes,
+                 activation, activation_args, dropout_probability=0., batch_normalization=False,
+                 initialization_args=None):
 
-        super(MultiInstanceLearningModel, self).__init__()
+        super(TransformerClassificationHead, self).__init__()
 
-        if model_module == 'timm':
-            self.backbone = timm.create_model(
-                model_name=model_name,
-                pretrained=pretrained,
-                num_classes=head_args['n_classes']
-            )
-            n_classifier_features = self.backbone.get_classifier().in_features
-        else:
-            raise ValueError(f'Invalid model_module {model_module}')
+        self.classifier = nn.Sequential(
+            nn.Linear(input_features, intermediate_features, bias=True),
+            getattr(nn, activation)(**activation_args),
+            nn.BatchNorm1d(num_features=intermediate_features) if batch_normalization else nn.Identity(),
+            nn.Dropout(p=dropout_probability) if dropout_probability >= 0. else nn.Identity(),
+            nn.Linear(intermediate_features, n_classes, bias=True),
+            nn.Softmax(dim=-1) if n_classes > 1 else nn.Identity()
+        )
 
-        self.classification_head = ClassificationHead(input_features=n_classifier_features * n_instances, **head_args)
+        if initialization_args is not None:
+            for module in self.classifier:
+                if isinstance(module, nn.Linear) or isinstance(module, nn.BatchNorm1d):
+                    initialize_weights(module=module, **initialization_args)
+
+    def forward(self, x):
+
+        output = self.classifier(x)
+
+        return output
+
+
+class ConvolutionalMultiInstanceLearningModel(nn.Module):
+
+    def __init__(self, n_instances, model_name, pretrained, head_class, head_args):
+
+        super(ConvolutionalMultiInstanceLearningModel, self).__init__()
+
+        self.backbone = timm.create_model(
+            model_name=model_name,
+            pretrained=pretrained,
+            num_classes=head_args['n_classes']
+        )
+        n_classifier_features = self.backbone.get_classifier().in_features
+        self.classification_head = eval(head_class)(input_features=n_classifier_features * n_instances, **head_args)
 
     def forward(self, x):
 
@@ -75,6 +101,36 @@ class MultiInstanceLearningModel(nn.Module):
         # Stack feature maps on channel dimension before passing feature maps to classification head
         feature_batch_size, feature_channel, feature_height, feature_width = x.shape
         x = x.contiguous().view(input_batch_size, feature_channel * input_instance, feature_height, feature_width)
+        output = self.classification_head(x)
+
+        return output
+
+
+class TransformerMultiInstanceLearningModel(nn.Module):
+
+    def __init__(self, n_instances, model_name, pretrained, head_class, head_args):
+
+        super(TransformerMultiInstanceLearningModel, self).__init__()
+
+        self.backbone = timm.create_model(
+            model_name=model_name,
+            pretrained=pretrained,
+            num_classes=head_args['n_classes']
+        )
+        n_classifier_features = self.backbone.get_classifier().in_features
+        self.backbone.head = nn.Identity()
+        self.classification_head = eval(head_class)(input_features=n_classifier_features * n_instances, **head_args)
+
+    def forward(self, x):
+
+        # Stack instances on batch dimension before passing input to feature extractor
+        input_batch_size, input_instance, input_channel, input_height, input_width = x.shape
+        x = x.view(input_batch_size * input_instance, input_channel, input_height, input_width)
+        x = self.backbone(x)
+
+        # Stack feature maps on channel dimension before passing feature maps to classification head
+        feature_batch_size, feature_count = x.shape
+        x = x.contiguous().view(input_batch_size, feature_count * input_instance)
         output = self.classification_head(x)
 
         return output
