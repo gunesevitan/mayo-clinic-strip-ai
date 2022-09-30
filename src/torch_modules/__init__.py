e34c09c2de316ee3e6228f280ba54111bbed0ed3
gunesevitan <gunesevitan@gmail.com>
2022-09-30 20:23:28 +0300

    init

--- src/torch_modules/__init__.py
+++ src/torch_modules/__init__.py
@@ -1,4 +1,4 @@
 from torch.nn import *
 
-from .loss_functions import FocalLoss, LabelSmoothingBCEWithLogitsLoss, MacroBCEWithLogitsLoss
+from .loss_functions import FocalLoss, LabelSmoothingBCEWithLogitsLoss, MacroBCEWithLogitsLoss, WeightedLogLoss
 from .mil import ConvolutionalMultiInstanceLearningModel, TransformerMultiInstanceLearningModel
